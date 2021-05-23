0ec09b65352b1a0ffaaad688d4ffcb95024bd968
Mohammed <mobeelco@gmail.com>
2021-05-23 23:13:53 +0300

    customize route name

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use Imdhemy\Purchases\Http\Controllers\ServerNotificationController;
 
 Route::post(
-    '/api/google-webhook',
+    '/google-webhook',
     [ServerNotificationController::class, 'google']
 )->name('purchase.serverNotifications.google');
 
 Route::post(
-    '/api/apple-webhook',
+    '/apple-webhook',
     [ServerNotificationController::class, 'apple']
 )->name('purchase.serverNotifications.apple');
